1411fe486d00a5f547b30357388b06db592b2d86
pdhruv93 <38987871+pdhruv93@users.noreply.github.com>
2019-10-23 13:12:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,57 @@
 
 from flask import Flask
 
+from selenium import webdriver
+import os
+import time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+skypeurl = 'https://web.skype.com/'
+skypeusername = 'dhruv.pandey5'
+skypepassword = 'dhruv.pandey5'
+
+while True:
+    try:
+        driver.find_element_by_id('idSIButton9')
+        
+        #its not login. so perform login
+        print("Performing Login to Skype")
+        driver.find_element_by_id('i0116').send_keys(skypeusername)
+        time.sleep(2)
+        driver.find_element_by_id('idSIButton9').click()
+        time.sleep(2)
+        driver.find_element_by_id('i0118').send_keys(skypepassword)  
+        time.sleep(2)
+        driver.find_element_by_id('idSIButton9').click()
+        time.sleep(6)
+    except:
+         #its already login. refresh and get count
+        driver.refresh()
+        time.sleep(6)
+        
+        try:
+            temp1=driver.title.split(')')[0]
+            temp2=temp1.split('(')[1]
+        
+            skypeNotifications=temp2
+        except:
+            skypeNotifications=0
+            
+        if not skypeNotifications:
+            skypeNotifications=0
+        
+        print("{}:".format(skypeNotifications))
+        time.sleep(6)
+
+
+
 
 # In[ ]:
 
